7220abf45563fc49d323dad6d72deac47d5a6116
Zach Pflederer <zach@giveforward.com>
2015-02-14 18:21:52 -0600

    Reader module to output array of person-like hashes

---
+++ reader.rb
@@ -0,0 +1,33 @@
+module Reader
+	require 'csv'
+
+	def self.read(path)
+		output = []
+		CSV.foreach(path) do |row|
+			output << format_data(parse_row(row))
+		end
+		output
+	end
+
+	def self.format_data(data)
+		{
+			lastname: data[0],
+			firstname: data[1],
+			gender: data[2],
+			favorite_color: data[3],
+			birthday: data[4]
+		}
+	end
+
+	def self.parse_row(row)
+		row = (row.length > 1 ? row.join(',') : row[0])
+		if row.include?('|')
+			data = row.split(' | ')
+		elsif row.include?(',')
+			data = row.split(', ')
+		else
+			data = row.split(' ')
+		end
+		return data
+	end
+end
---
+++ spec/reader_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../config.rb'
+
+describe Reader do
+	let(:pipe_row) { ["Bartell | Carmela | male | yellow | 1996-06-06"] }
+	let(:comma_row) { ["Bartell, Carmela, male, yellow, 1996-06-06"] }
+	let(:space_row) { ["Bartell Carmela male yellow 1996-06-06"] }
+
+	describe '#parse_row' do
+		it 'converts rows into similar Array format' do
+			expect(Reader.parse_row(comma_row)).to eq(Reader.parse_row(pipe_row))
+			expect(Reader.parse_row(comma_row)).to eq(Reader.parse_row(space_row))
+		end
+	end
+
+	describe '#read' do
+		it 'returns an array of hashes from a CSV file' do
+			file_path = './csv/pipe.csv'
+			expect(Reader.read(file_path).length).to eq 10
+			expect(Reader.read(file_path)).to be_a Array
+			expect(Reader.read(file_path)[0]).to be_a Hash
+		end
+	end
+
+	describe '#format_data' do
+		it 'converts a raw array of data to a hash with meaningful keys' do
+			data = Reader.parse_row(pipe_row)
+			expect(Reader.format_data(data)).to eq({lastname: data[0],
+																							firstname: data[1],
+																							gender: data[2],
+																							favorite_color: data[3],
+																							birthday: data[4] })
+		end
+	end
+end
