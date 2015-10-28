21351feadb68a9129efbd7a9f2c129ebcb39b854
Zach Pflederer <zach@giveforward.com>
2015-10-28 17:23:51 -0500

    bugfix

--- seed.rb
+++ seed.rb
@@ -7,9 +7,9 @@ it_already_has_been_seeded = CSV.read(DATA_PATH).length > 1
 def seed_csv_with_person_data
   CSV.open(DATA_PATH, "wb") do |csv|
     10.times do
-      csv << ["#{Faker::Name.last_name}, #{Faker::Name.first_name}, #{['male', 'female'].sample}, #{['red', 'yellow', 'blue', 'green', 'orange'].sample}, #{Faker::Date.birthdate.to_s.split('-').rotate.join('/')}"]
-      csv << ["#{Faker::Name.last_name} | #{Faker::Name.first_name} | #{['male', 'female'].sample} | #{['red', 'yellow', 'blue', 'green', 'orange'].sample} | #{Faker::Date.birthdate.to_s.split('-').rotate.join('/')}"]
-      csv << ["#{Faker::Name.last_name} #{Faker::Name.first_name} #{['male', 'female'].sample} #{['red', 'yellow', 'blue', 'green', 'orange'].sample} #{Faker::Date.birthdate.to_s.split('-').rotate.join('/')}"]
+      csv << ["#{Faker::Name.last_name}, #{Faker::Name.first_name}, #{['male', 'female'].sample}, #{['red', 'yellow', 'blue', 'green', 'orange'].sample}, #{Faker::Date.birthday.to_s.split('-').rotate.join('/')}"]
+      csv << ["#{Faker::Name.last_name} | #{Faker::Name.first_name} | #{['male', 'female'].sample} | #{['red', 'yellow', 'blue', 'green', 'orange'].sample} | #{Faker::Date.birthday.to_s.split('-').rotate.join('/')}"]
+      csv << ["#{Faker::Name.last_name} #{Faker::Name.first_name} #{['male', 'female'].sample} #{['red', 'yellow', 'blue', 'green', 'orange'].sample} #{Faker::Date.birthday.to_s.split('-').rotate.join('/')}"]
     end
   end
 end
